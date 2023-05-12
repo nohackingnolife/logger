47ca1674c68f15f330b26a9a5eb2eeda1e947477
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2023-05-12 16:22:16 +0200

    Bump to v2

--- logger.go
+++ logger.go
@@ -6,6 +6,14 @@ func Info(data interface{}) {
 	fmt.Println("Info: ", data)
 }
 
+func Warning(data interface{}) {
+	fmt.Println("Warning: ", data)
+}
+
 func Error(data interface{}) {
 	fmt.Println("Error: ", data)
+}
+
+func Fatal(data interface{}) {
+	fmt.Println("Fatal: ", data)
 }
